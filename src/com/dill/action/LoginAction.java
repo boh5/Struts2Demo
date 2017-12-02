a33d41b1e962dbbd2bb5c9480160142ea16edebf
dill <bohuang1994@gmail.com>
2017-12-02 15:36:51 +0800

    show how to accessing Servlet API by Action

--- src/com/dill/action/LoginAction.java
+++ src/com/dill/action/LoginAction.java
@@ -25,10 +25,21 @@ public class LoginAction extends ActionSupport {
 
     @Override
     public String execute() throws Exception {
+        ActionContext context = ActionContext.getContext();
+        Integer counter = (Integer) context.getApplication().get("counter");
+        if (counter == null) {
+            counter = 1;
+        } else {
+            counter = counter + 1;
+        }
+        context.getApplication().put("counter", counter);
+        context.getSession().put("user", getUsername());
         if (getUsername().equals("dill") && getPassword().equals("233")) {
-            ActionContext.getContext().getSession().put("user", getUsername());
+//            ActionContext.getContext().getSession().put("user", getUsername());
+            context.put("tip", "服务器提示：您已经成功登录");
             return SUCCESS;
         }
+        context.put("tip", "服务器提示：登录失败");
         return ERROR;
     }
 }
